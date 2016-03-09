1f5e6b8db2d86d4606e56995979157e7a035b53b
19mariusz95 <19mariusz95@gmail.com>
2016-03-09 22:06:41 +0100

    zad2 arguments

--- zad2/main.c
+++ zad2/main.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+
+void find(char *path, char *privileges);
+
 int main(int argc, char *argv[]) {
-    
+    if (argc < 3) {
+        printf("%s\n", "You have to input all arguments");
+        return 1;
+    }
+    char *path = argv[1];
+    char *access_privileges = argv[2];
+
+    find(path, access_privileges);
+
     return 0;
+}
+
+void find(char *path, char *privileges) {
+    
 }
