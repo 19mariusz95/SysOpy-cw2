0759516077aa8c919d32db6848e86accbb12e84f
19mariusz95 <19mariusz95@gmail.com>
2016-03-09 23:07:50 +0100

    zad2 not work yet

--- zad2/main.c
+++ zad2/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
 void find(char *path, char *privileges);
 
@@ -10,11 +14,49 @@ int main(int argc, char *argv[]) {
     char *path = argv[1];
     char *access_privileges = argv[2];
 
+    if (strlen(access_privileges) != 9) {
+        printf("Trolollo");
+        exit(1);
+    }
+
     find(path, access_privileges);
 
     return 0;
 }
 
 void find(char *path, char *privileges) {
-    
+
+    printf("%s\n", path);
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        ///
+        return;
+    }
+    struct stat filestat;
+    stat(path, &filestat);
+
+    size_t len = strlen(path);
+
+    char fn[FILENAME_MAX];
+    strcpy(fn, path);
+    fn[len++] = '/';
+
+    struct dirent *ent;
+
+    while ((ent = readdir(dir)) != NULL) {
+        if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, ".."))
+            continue;
+        if (S_ISDIR(filestat.st_mode)) {
+            strncpy(fn + len, ent->d_name, FILENAME_MAX - len);
+            find(fn, privileges);
+        }
+        else if (S_ISREG(filestat.st_mode) == 1) {
+            printf("%s\n", ent->d_name);
+        }
+    }
+    closedir(dir);
+}
+
+void fun1(struct dirent *ent) {
+    printf("%s\n", ent->d_name);
 }
